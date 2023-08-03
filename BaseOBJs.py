5318fced4d18de1684621a4d17c6f7d301b1ffa8
Yanarp Madnug <pranaygundam00@gmail.com>
2023-08-02 20:32:25 -0400

    planning

--- BaseOBJs.py
+++ BaseOBJs.py
@@ -1,6 +1,28 @@
 #
 
+class Room:
+    def __init__(self, name):
+        self.name = name
+
 
 class Player:
     def __init__(self, name):
         self.name = name
+
+
+class NPC:
+    def __init__(self, name):
+        self.name = name
+
+class Enemy(NPC):
+    def __init__(self, name, hp):
+        super().__init__(name)
+        self.hp = hp
+        self.hitbox = {}
+
+
+
+class Melee_Enemy(Enemy):
+    def __init__(self, name):
+        hp = 20
+        super().__init__(name, hp)
